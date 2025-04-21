9e18ddb46bcfc30630a85b41aa4f75250e92717c
andersonlrsilva <andersonlrsilva23@gmail.com>
2025-04-21 19:44:01 -0300

    1

---
+++ aula_289.py
@@ -0,0 +1,34 @@
+
+import json
+from pprint import pprint
+
+
+string_json = '''
+ {
+   "title": "O Senhor dos Anéis: A Sociedade do Anel",
+   "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+   "is_movie": true,
+   "imdb_rating": 8.8,
+   "year": 2001,
+   "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+   "budget": null
+ }
+'''
+
+
+
+
+filme = json.loads(string_json)
+pprint(filme)
+
+
+
+
+
+
+
+
+
+
+
+
