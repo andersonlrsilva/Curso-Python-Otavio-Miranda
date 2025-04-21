680ed34ee939a7048d958ab2908fbb2c55d54456
andersonlrsilva <andersonlrsilva23@gmail.com>
2025-04-21 17:09:43 -0300

    Add gitignore

--- Aula_171.py
+++ Aula_171.py
@@ -2,12 +2,12 @@ lista_a = [12, 2, 3, 47, 5, 6, 7]
 lista_b = [1, 24, 3, 4, 7]
 
 
-def somalista(l1, l2):
+def somalista(l1, l2): # type: ignore
     soma = []
     # intervalo = min(len(l1), len(l2))
-    for i in range(min(len(l1), len(l2))):
-        soma.append(l1[i] + l2[i])
-    print(soma)
+    for i in range(min(len(l1), len(l2))):# type: ignore
+        soma.append(l1[i] + l2[i]) # type: ignore
+    print(soma) # type: ignore
 
 
 somalista(lista_a, lista_b)
