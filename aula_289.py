26ec36805cf7210e0cf4636defbf3f64ddd11109
andersonlrsilva <andersonlrsilva23@gmail.com>
2025-04-21 23:27:52 -0300

    1

--- aula_289.py
+++ aula_289.py
@@ -1,6 +1,19 @@
 
 import json
 from pprint import pprint
+from typing import TypedDict
+
+# Criando classes para tipagem
+
+
+class Movie(TypedDict):
+    title: str
+    original_title: str
+    is_movie: bool
+    imdb_rating: float
+    year: int
+    characters: list[str]
+    budget: None | float
 
 
 string_json = '''
@@ -15,20 +28,6 @@ string_json = '''
  }
 '''
 
-
-
-
-filme = json.loads(string_json)
-pprint(filme)
-
-
-
-
-
-
-
-
-
-
-
-
+filme: Movie = json.loads(string_json)
+pprint(filme["title"])
+print("Ano de lançamento", filme['year'])
