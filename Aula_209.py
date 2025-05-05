0043482bc57c66d2c18b72ed875b05e85e383087
andersonlrsilva23@gmail.com <andersonlrsilva23@gmail.com>
2025-05-05 19:04:01 -0300

    11212

--- Aula_209.py
+++ Aula_209.py
@@ -1,30 +1,24 @@
 class Pessoa:
     ano = 2023
 
-
-    def __init__(self, nome, idade, peso, altura):
+    def __init__(self, nome, idade, peso, altura):  # type: ignore
         self.nome = nome
         self.idade = idade
         self.peso = peso
         self.altura = altura
 
     @classmethod
-    def saudacao(cls, nome):
-        #print(f'Olá {nome}')
+    def saudacao(cls, nome):  # type: ignore
+        # print(f'Olá {nome}')
         return f'Olá {nome}'
-    
+
     @classmethod
-    def imc(cls, peso, altura):
-        imc = peso / (altura ** 2)
+    def imc(cls, peso, altura):  # type: ignore
+        imc = peso / (altura ** 2)  # type: ignore
         return f'Olá {imc}'
 
 
-
-
-
-
-
-#p1 = Pessoa('João', 34)
-#print(p1.nome, p1.idade)
-saudacao = Pessoa.imc(80, 1.70)
+# p1 = Pessoa('João', 34)
+# print(p1.nome, p1.idade)
+saudacao = Pessoa.imc(80, 1.70)  # type: ignore
 print(saudacao)
