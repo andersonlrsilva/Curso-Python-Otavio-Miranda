29b45d931a107596616bea1d4a36e3bdde62200d
andersonlrsilva <andersonlrsilva23@gmail.com>
2025-05-27 20:49:59 -0300

    q

--- aula_340/aula_004.py
+++ aula_340/aula_004.py
@@ -9,10 +9,15 @@
 #   -> show
 # -> exec
 
-from PySide6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QHBoxLayout, QGridLayout, QWidget
+from PySide6.QtWidgets import (QApplication, QPushButton, QMainWindow,
+                               QVBoxLayout, QHBoxLayout, QGridLayout, QWidget)
 import sys
 
-app = QApplication()
+app = QApplication(sys.argv)
+window = QMainWindow()
+central_widget = QWidget()
+window.setCentralWidget(central_widget)
+window.setWindowTitle('Minha Janela')
 
 
 # Botões
@@ -25,8 +30,6 @@ botao2.setStyleSheet('font-size: 40px')
 botao3 = QPushButton('texto do botão')
 botao3.setStyleSheet('font-size: 40px')
 
-# widget Central (Janela principal da aplicação)
-central_widget = QWidget()
 
 layout = QGridLayout()
 central_widget.setLayout(layout)
@@ -35,5 +38,27 @@ layout.addWidget(botao1, 1, 1, 1, 1)
 layout.addWidget(botao2, 1, 2, 1, 1)
 layout.addWidget(botao3, 2, 1, 1, 2)
 
-central_widget.show()
+# primeira ação menu
+
+
+def slot_example(status_bar):
+    print(123)
+    status_bar.showMessage('O meu slot foi executado')
+
+
+# statusbar
+status_bar = window.statusBar()
+status_bar.showMessage('Mostrar mensagem na barra')
+
+# menubar
+menu = window.menuBar()
+primeiro_menu = menu.addMenu("Arquivo")
+primeira_acao = primeiro_menu.addAction('Imprimir')
+segunda_acao = primeiro_menu.addAction('Excluir')
+primeira_acao.triggered.connect(lambda: slot_example(status_bar))
+
+segundo_menu = menu.addMenu("Editar")
+primeira_acao = segundo_menu.addAction('Cortar')
+
+window.show()
 app.exec()
